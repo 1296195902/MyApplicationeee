d696365ca640d316e31269ab479443f317f3b1a8
1296195902 <1296195902@qq.com>
2017-08-11 08:18:33 +0800

    提交代码

--- app/src/main/java/com/example/admin/myapplicationeee/MainActivity.java
+++ app/src/main/java/com/example/admin/myapplicationeee/MainActivity.java
@@ -9,6 +9,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+//        alkldjflasdf
 //        laksdhfkashdfakshdf
         int a = 8;
 //        skljdfajdf
