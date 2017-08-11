fe7a5988ca4d9f792ac7392ee552daaf218f9ef2
1296195902 <1296195902@qq.com>
2017-08-11 08:26:47 +0800

    111

--- app/src/main/java/com/example/admin/myapplicationeee/MainActivity.java
+++ app/src/main/java/com/example/admin/myapplicationeee/MainActivity.java
@@ -9,11 +9,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-//        alkldjflasdf
-//        laksdhfkashdfakshdf
-//        ksjdfsdf
-
-//        skjdfsd
+//
+//
         int a = 8;
 //        skljdfajdf
 //        ;sjdfksjdfsdf
