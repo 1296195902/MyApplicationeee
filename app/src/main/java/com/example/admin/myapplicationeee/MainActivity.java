eadc31bc89a0a43ae5e2fe01c385115a884f174e
1296195902 <1296195902@qq.com>
2017-08-11 08:15:33 +0800

    asdfasdasdfadsf alsdfjadfaf

--- app/src/main/java/com/example/admin/myapplicationeee/MainActivity.java
+++ app/src/main/java/com/example/admin/myapplicationeee/MainActivity.java
@@ -11,6 +11,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 //        laksdhfkashdfakshdf
         int a = 8;
+//        skljdfajdf
 //        ;sjdfksjdfsdf
 //        sjkdkfsdf
     }
